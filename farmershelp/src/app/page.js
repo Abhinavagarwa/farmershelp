cbd234fb8efd14065ea519ecd3a646b430215599
Sneha Joshi <136231083+SJ6github@users.noreply.github.com>
2024-08-14 11:45:05 +0530

    Update page.js

--- farmershelp/src/app/page.js
+++ farmershelp/src/app/page.js
@@ -1,18 +1,20 @@
 import Button from '../components/button'
 import Styles from'./page.module.css'
+
+
 export default function Home() {
     return (
         <div className={Styles.container}>
-            <div className={Styles.main1}>
-            <h1 className={Styles.head}>WELCOME,</h1>
-            <h2 className={Styles.head1}>TO THE FARMERSHELP</h2>
-            <p className={Styles.para}>Welcome to our Farmer's Help website, your trusted resource for agricultural guidance and support. <br></br>Whether you're just starting out or have years of experience, our platform is designed to provide you with the latest information, tools, and resources to help you succeed in your farming journey. <br></br>From crop management tips to government schemes updates and market prices or MSP, we are here to assist you in every step of the way.<br></br> Get started today by exploring our resources and connecting with experts who can help you grow and thrive.</p>
+            <div className={Styles.main1}> 
+            <img src="C:\Users\hp\Desktop\farmershelp-main\farmershelp\src\images" alt="FarmersHelp Logo" className={Styles.logo} />
+            <h1 className={Styles.head}>FarmersHelp</h1>
+            <p className={Styles.para}>Welcome to our Farmer's Help website, your trusted resource for agricultural guidance and support. Whether you're just starting out or have years of experience, our platform is designed to provide you with the latest information, tools, and resources to help you succeed in your farming journey. From crop management tips to government schemes updates and market prices or MSP, we are here to assist you in every step of the way. Get started today by exploring our resources and connecting with experts who can help you grow and thrive.</p>
                         <br></br>
             <Button/>
             <p className={Styles.links}>📞 +91 9999999999<br></br>gmail:abc@gmail.com</p>
             </div>
             <br></br>
-            <p className={Styles.contributors}>This Project is created by<br></br>
+            <p className={Styles.contributors}>This Project is created by:<br></br>
                Abhinav Agarwal<br></br>
                Devansh Aggarwal<br></br>
                Sneha Joshi<br></br>
