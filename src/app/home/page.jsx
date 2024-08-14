596190a7b83485cdc30724b9a5c564b9dd140a90
Abhinav Agarwal <119868907+Abhinavagarwa@users.noreply.github.com>
2024-08-14 11:57:37 +0000

    changed

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 export default function LoginPage(){
     return(
         <div>
-      <div class="topnav">
-        <h1 className="head">FarmersHelp</h1>
+      <div class='topnav'>
+        <h1 className='head'>FarmersHelp</h1>
         <div>
-          <Link href="/home" className="active">Home</Link>
-          <Link href="/aboutus">About us</Link>
-          <Link href="/organizations">Organizations</Link>   
-          <Link href="/schemes">Schemes</Link>   
-          <Link href="/solutions">Solution</Link>         
+          <Link href='/home' className='active'>Home</Link>
+          <Link href='/aboutus'>About us</Link>
+          <Link href='/organizations'>Organizations</Link>   
+          <Link href='/schemes'>Schemes</Link>   
+          <Link href='/solutions'>Solution</Link>         
         </div>
       </div>
             <div className='into1'>     
@@ -25,42 +25,42 @@ export default function LoginPage(){
             <br></br>
             <div className='testimonial-card'>
                 <h1 className='test1'>Ramesh Kumar</h1>
-                <p>"As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
+                <p>'As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
 
-                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support."<br></br>
+                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support.'<br></br>
                  <br></br>
                 — <b>Ramesh Kumar, Farmer from Punjab</b></p>
             </div>
             
             <div className='testimonial-card'>
                 <h1 className='test1'>Mohd. Rizwan</h1>
-                <p>"As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
+                <p>'As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
 
-                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support."<br></br>
+                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support.'<br></br>
                  <br></br>
                 —  <b>Mohd. Rizwan, Farmer from Haryana</b></p>
             </div>
             
             <div className='testimonial-card'>
                 <h1 className='test1'>Vinod Kumar</h1>
-                <p>"As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
+                <p>'As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
 
-                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support."<br></br>
+                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support.'<br></br>
                  <br></br>
                 — <b>Vinod Kumar, Farmer from Uttar Pradesh</b></p>
             </div>
 
             <div className='testimonial-card'>
                 <h1 className='test1'>Vikas Yadav</h1>
-                <p>"As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
+                <p>'As a third-generation farmer, I’ve faced my share of challenges, but nothing prepared me for the financial struggles brought on by unpredictable weather and market fluctuations. I was on the brink of giving up when I found Farmers Help. The resources they provided on securing loans and grants were invaluable, and their sustainability guides helped me transform my farming practices.<br></br>
 
-                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support."<br></br>
+                Today, not only am I financially stable, but my farm is thriving with eco-friendly methods that have reduced costs and improved yields. Farmers Help didn’t just save my farm—they gave me the tools to build a brighter future for my family and community. I’m forever grateful for their support.'<br></br>
                  <br></br>
                 —  <b>Mohd. Rizwan, Farmer from Bihar</b></p>
             </div>
             </div>
             
-            <p className='links'>📞 +91 9999999999{"        "}gmail:abc@gmail.com</p>
+            <p className='links'>📞 +91 9999999999{'        '}gmail:abc@gmail.com</p>
         </div>
     )
 }
